965b723274065cbe7e99ddd69f1fb82b783a6d5e
Syed Zohaib Ali <95756200+ZohaiAli@users.noreply.github.com>
2025-11-05 21:05:07 +0500

    Update Bill.aspx.cs
    i change only design ok

--- Code/DBProject/Doctor/Bill.aspx.cs
+++ Code/DBProject/Doctor/Bill.aspx.cs
@@ -1,59 +1,76 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
-using DBProject.DAL;
+using System;
 using System.Data;
+using System.Web.UI;
+using DBProject.DAL;
 
 namespace doctor
 {
-    public partial class bill : System.Web.UI.Page
+    public partial class bill : Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
-            myDAL objmyDAL = new myDAL();
-            DataTable dt = new DataTable();
-            int found;
-
-            int did = (int)Session["idoriginal"];
-            
-            found = objmyDAL.generate_bill_DAL(did, ref dt);
-
-            if (found != 1)
-            { Response.Write("<script>alert('There was some error');</script>"); }
-            else
+            if (!IsPostBack)
             {
-                Label1.Text = dt.Rows[0][0].ToString();
+                try
+                {
+                    int did = (int)Session["idoriginal"];
+                    myDAL objmyDAL = new myDAL();
+                    DataTable dt = new DataTable();
+                    int found = objmyDAL.generate_bill_DAL(did, ref dt);
+
+                    if (found != 1 || dt.Rows.Count == 0)
+                    {
+                        Label1.Text = "Error generating bill";
+                        Response.Write("<script>alert('There was some error generating the bill.');</script>");
+                    }
+                    else
+                    {
+                        Label1.Text = dt.Rows[0][0].ToString();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Response.Write($"<script>alert('Error: {ex.Message}');</script>");
+                }
             }
         }
 
-
         public void bill_paid(object sender, EventArgs e)
         {
-            myDAL objmyDAL = new myDAL();
-            
-            int  did = (int)Session["idoriginal"];
-            int appoint = (int)Session["appointid"];
-            objmyDAL.paid_bill_DAL(did,appoint);
+            try
+            {
+                int did = (int)Session["idoriginal"];
+                int appoint = (int)Session["appointid"];
 
-			Response.BufferOutput = false;
-            Response.Redirect("patienthistory.aspx");
+                myDAL objmyDAL = new myDAL();
+                objmyDAL.paid_bill_DAL(did, appoint);
+
+                Response.BufferOutput = false;
+                Response.Redirect("patienthistory.aspx");
+            }
+            catch (Exception ex)
+            {
+                Response.Write($"<script>alert('Error: {ex.Message}');</script>");
+            }
         }
-
 
         public void bill_Unpaid(object sender, EventArgs e)
         {
-            myDAL objmyDAL = new myDAL();
+            try
+            {
+                int did = (int)Session["idoriginal"];
+                int appoint = (int)Session["appointid"];
 
-            int did = (int)Session["idoriginal"];
-            int appoint = (int)Session["appointid"];
-            objmyDAL.Unpaid_bill_DAL(did, appoint);
+                myDAL objmyDAL = new myDAL();
+                objmyDAL.Unpaid_bill_DAL(did, appoint);
 
-            Response.BufferOutput = false;
-            Response.Redirect("patienthistory.aspx");
+                Response.BufferOutput = false;
+                Response.Redirect("patienthistory.aspx");
+            }
+            catch (Exception ex)
+            {
+                Response.Write($"<script>alert('Error: {ex.Message}');</script>");
+            }
         }
     }
 }
